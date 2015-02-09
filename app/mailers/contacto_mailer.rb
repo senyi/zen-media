055f074d54689b34d3d1733b7cf24f36f4602341
Senyi <senyib@gmail.com>
2015-02-09 14:12:58 -0600

    agregue cc de mailer

--- app/mailers/contacto_mailer.rb
+++ app/mailers/contacto_mailer.rb
@@ -2,6 +2,6 @@ class ContactoMailer < ActionMailer::Base
 
   def contacto(contacto)
     @contacto = contacto
-    mail to: "contacto.zmedia@gmail.com", subject: "Contacto a Zen Media", from: "contacto.zmedia@gmail.com"
+    mail to: "contacto.zmedia@gmail.com", cc: ["jdsampayo@gmail.com","senyib@gmail.com"], subject: "Contacto a Zen Media", from: "contacto.zmedia@gmail.com"
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-    ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.zenmedia.mx",
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "zenmedia.mx",
     :user_name            => "contacto.zmedia@gmail.com",
     :password             => "zenmedia.-*",
-    :authentication       => "login",
+    :authentication       => :plain,
     :enable_starttls_auto => true
   }
 
